73158474bb53fbb6cff4d5815c504c860f4f7188
Flora Petersen <floramarina@comcast.net>
2020-06-06 04:31:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(integer)
   if integer <= 1
     return false
   else
-    if (2..n).any? {|i| integer % i == 0}
+    if (2..integer).any? {|i| integer % i == 0}
       return false
     else
       return true
